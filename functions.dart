876eef8fb8d9dc7a4775f0d1759baef49a718a1c
Fardeen Khan <khanfardeen9983@gmail.com>
2019-06-16 11:46:33 +0530

    Fat arrow notation and types of parametes

--- functions.dart
+++ functions.dart
@@ -7,16 +7,22 @@ void getParamter() {
 
 main(List<String> args) {
   getParamter();
-  print("Area : ${getArea(length: 5, breadth: 6)}");
+  print("Area : ${getArea(5)}");
   print("Volume : ${getVolume(5, 6, 7)}");
+  cityNames("Anand", "Ahmedabad");
+  cityNames("Anand", "Ahmedabad","Vadodara");
 }
 
-int getArea({int length = 5, int breadth = 5}) {
+int getArea(int length, {int breadth = 5}) {
   int area = length * breadth;
   return area;
 }
 
-int getVolume(int length, int breadth, int height) {
-  int volume = length * breadth * height;
-  return volume;
+int getVolume(int length, int breadth, int height) => length * breadth * height;
+
+void cityNames(String s1, String s2, [String s3]) {
+  print("City name 1 : $s1");
+  print("City name 2 : $s2");
+  print("City name 3 : $s3");
+  print("");
 }
